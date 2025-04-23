5c267d12fdbcafc804bbb17180e391373b119203
XVPB511 <por4987@gmail.com>
2025-04-23 11:56:38 +0700

    11:56-23-4

--- components/socket.ts
+++ components/socket.ts
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
+import { useRuntimeConfig } from '#imports'
 
-const URL = 'http://localhost:3002';
-// const token = sessionStorage.getItem("token")
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJ1c2VybmFtZSI6ImphbmVfc21pdGgiLCJlbXBfY29kZSI6IkVNUDAwOSIsInVzZXJfY3JlYXRlZCI6IjIwMjUtMDQtMTZUMDM6MjE6MjUuNzIwWiIsImlhdCI6MTc0NTIxMTk0OCwiZXhwIjoxNzQ1MjQ3OTQ4fQ.MDfEpKlapaGTFkjZ1ntBqv7bEMubCyaLfeAeFcghASc"
+const config = useRuntimeConfig();
+const token = sessionStorage.getItem("token")
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJ1c2VybmFtZSI6ImphbmVfc21pdGgiLCJlbXBfY29kZSI6IkVNUDAwOSIsInVzZXJfY3JlYXRlZCI6IjIwMjUtMDQtMTZUMDM6MjE6MjUuNzIwWiIsImlhdCI6MTc0NTI5Mzk3NiwiZXhwIjoxNzQ1MzI5OTc2fQ.9i4NU7q0nUDTVqxby8-N9wXjd9Y5NE6XDk8Q271MQ-Y"
 console.log("token ", token)
 
 const socketOptions = {
@@ -13,10 +14,10 @@ const socketOptions = {
   }
 };
 
-export const socket = io(URL + '/socket/all', socketOptions);
-export const socketpart = io(URL + '/socket/part', socketOptions);
-export const socketvat = io(URL + '/socket/vat', socketOptions);
-export const socketprint = io(URL + '/invoice/print/:sh_running', {
+export const socket = io(config.public.apiBase + '/socket/all', socketOptions);
+export const socketpart = io(config.public.apiBase + '/socket/part', socketOptions);
+export const socketvat = io(config.public.apiBase + '/socket/vat', socketOptions);
+export const socketprint = io(config.public.apiBase + '/invoice/print/:sh_running', {
   ...socketOptions,
   path: '/socket.io' // ใช้ path เฉพาะสำหรับ print
 });
