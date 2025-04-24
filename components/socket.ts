34e7b62793e8df7ff02301b32701aff48339fbc5
6710110400 SASIT SUANSADANG <155415427+Sasit-Nine@users.noreply.github.com>
2025-04-24 14:50:59 +0700

    24-4- no.4

--- components/socket.ts
+++ components/socket.ts
@@ -2,8 +2,9 @@ import { io } from "socket.io-client";
 import { useRuntimeConfig } from '#imports'
 
 // const config = useRuntimeConfig();
-// const token = process.client ? sessionStorage.getItem("token") : null;
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJ1c2VybmFtZSI6ImphbmVfc21pdGgiLCJlbXBfY29kZSI6IkVNUDAwOSIsInVzZXJfY3JlYXRlZCI6IjIwMjUtMDQtMTZUMDM6MjE6MjUuNzIwWiIsImlhdCI6MTc0NTQ2Mzg2NCwiZXhwIjoxNzQ1NDk5ODY0fQ.nUzofjrk2XQ8Kw4Yxiwet_Oikmcer0bvpnKKzo6tf34"
+const token = process.client ? sessionStorage.getItem("token") : null;
+
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJ1c2VybmFtZSI6ImphbmVfc21pdGgiLCJlbXBfY29kZSI6IkVNUDAwOSIsInVzZXJfY3JlYXRlZCI6IjIwMjUtMDQtMTZUMDM6MjE6MjUuNzIwWiIsImlhdCI6MTc0NTQ2Mzg2NCwiZXhwIjoxNzQ1NDk5ODY0fQ.nUzofjrk2XQ8Kw4Yxiwet_Oikmcer0bvpnKKzo6tf34"
 // console.log("token ", token)
 
 const socketOptions = {
@@ -17,12 +18,14 @@ const socketOptions = {
 export const createSockets = () => {
   const config = useRuntimeConfig();
 
+  console.log('Log env: ',config.public.apiBase)
+  console.log('Log env: ',config.public.apiBaseInvoice)
 
   return {
-    socket: io(config.public.apiBase + '/socket/all', socketOptions),
-    socketpart: io(config.public.apiBase + '/socket/part', socketOptions),
-    socketvat: io(config.public.apiBase + '/socket/vat', socketOptions),
-    socketprint: io(config.public.apiBase + '/invoice/print/:sh_running', {
+    socket: io(config.public.apiBaseInvoice + '/socket/all', socketOptions),
+    socketpart: io(config.public.apiBaseInvoice + '/socket/part', socketOptions),
+    socketvat: io(config.public.apiBaseInvoice + '/socket/vat', socketOptions),
+    socketprint: io(config.public.apiBaseInvoice + '/invoice/print/:sh_running', {
       ...socketOptions,
       path: '/socket.io'
     })
